df85652f70ab26376e4deba113bf80210c30671e
tonchis <lucas.tolchinsky@gmail.com>
2012-01-16 15:00:12 -0300

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+gem 'mysql2'
+gem 'coffee-script'
+gem 'sass'
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'therubyracer'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Procon::Application.config.secret_token = 'ee14f48e91b44a96ba68c22e5644c2136e9c06666c825843f8da33d6adc81dc3a4c9409ba24734171b34e2ff5003905f60dedc5d5f5c459c11e6d125212cdaaa'
