767ab995978575288cd5e1302bb4f0fc02fb34ef
tonchis <lucas.tolchinsky@gmail.com>
2012-01-18 13:46:14 -0300

    Adds specs for lists_controller.

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -1,14 +1,39 @@
 require File.expand_path("spec/spec_helper")
 
 describe ListsController do
-  before do
+  before(:each) do
     @user = User.create username: "test", password: "test"
   end
 
   context "authentication" do
     it "should forbid requests without authentication" do
       get :index
-      puts response
+      response.code.should_not == "200"
+    end
+  end
+
+  describe "GET /lists" do
+    before do
+      login_user(@user)
+    end
+
+    it "should render the index template" do
+      get :index
+      response.should render_template(:index)
+    end
+  end
+
+  describe "POST /lists" do
+    before do
+      login_user(@user)
+      List.should_receive(:create).with(name: "new list", user: @user).
+        and_return({name: "new list"})
+    end
+
+    it "should create a new list" do
+      post :create, name: "new list"
+      response.code.should == "200"
+      response.body.should match(/\"name\":"new list\"/)
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,3 +26,6 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 end
+
+include Sorcery::TestHelpers::Rails
+
