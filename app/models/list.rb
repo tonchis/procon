60ebb8a7107c52ff19bc6d2db1c586c2e4b6246e
tonchis <lucas.tolchinsky@gmail.com>
2012-01-20 09:34:41 -0300

    Refactors list.rb.

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,6 @@ class List < ActiveRecord::Base
   validates_presence_of :name
   has_many :items
   accepts_nested_attributes_for :items, allow_destroy: true
-  attr_accessible :name, :items_attributes
+  # attr_accessible :name, :items_attributes, :user
 end
 
